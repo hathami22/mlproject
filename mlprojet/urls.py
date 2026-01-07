dd328eafbd0a902da55ecafeccc48e517c43e0ea
Mohamed Aziz Mokhtar <mohamedaziz.mokhtar@pristini-international.tn>
2026-01-07 23:03:17 +0100

    Add ML prediction features: Temporal Trajectory Clustering and Score Prediction pages

--- mlprojet/urls.py
+++ mlprojet/urls.py
@@ -21,4 +21,6 @@ from PublicpriveApp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('temporal-trajectory-clustering/', views.temporal_trajectory_clustering, name='temporal_trajectory_clustering'),
+    path('school-ranking-by-score/', views.school_ranking_by_score, name='school_ranking_by_score'),
 ]
