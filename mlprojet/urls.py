df721bd6f81353783aba04c0f34b19023f0e007b
yassin <yassinbenbelgacem001@gmail.com>
2026-01-08 22:05:55 +0100

    yassin models

---
+++ mlprojet/urls.py
@@ -0,0 +1,32 @@
+"""
+URL configuration for mlprojet project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from PublicpriveApp import views as public_views
+from YassinApp import views as viewsyassin
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', public_views.home, name='home'),
+    path('ObjectiveYassin2/', viewsyassin.objective_yassin2, name='objective_yassin2'),
+    path('ObjectiveYassin1/', viewsyassin.objective_yassin1, name='objective_yassin1'),
+    
+    path('api/extract_csv_data/', viewsyassin.extract_csv_data, name='extract_csv_data'),
+    path('api/run_prediction/', viewsyassin.run_prediction, name='run_prediction'),
+    path('api/extract_csv_data_yassin2/', viewsyassin.extract_csv_data_yassin2, name='extract_csv_data_yassin2'),
+    path('api/run_prediction_yassin2/', viewsyassin.run_prediction_yassin2, name='run_prediction_yassin2'),
+]
